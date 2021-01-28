2bfcf57914817375ac489481729b570f4e3666e9
gil <gil@MU53511.monmouth.edu>
2021-01-28 11:02:49 -0500

    Redo

--- HelloWorld/src/Hello.java
+++ HelloWorld/src/Hello.java
@@ -1,9 +1,8 @@
 
 public class Hello {
 
-	public static void main(String[] args) 
-	{
-	System.out.println("hello");	
+	public static void main(String[] args) {
+		System.out.println("Hello sir");
 
 	}
 
